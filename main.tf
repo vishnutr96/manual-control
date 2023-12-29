514cb99d6e172e226dcbb0aa00314902ae05fc31
vishnu <vishnu@gmail.com>
2023-12-29 07:47:42 +0000

    ftp port

--- main.tf
+++ main.tf
@@ -25,12 +25,21 @@ resource "aws_security_group" "http_access" {
 
 
   ingress {
+    from_port        = 21
+    to_port          = 21
+    protocol         = "tcp"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+ ingress {
     from_port        = 22
     to_port          = 22
     protocol         = "tcp"
     cidr_blocks      = ["0.0.0.0/0"]
     ipv6_cidr_blocks = ["::/0"]
   }
+
 
   ingress {
     from_port        = 80
